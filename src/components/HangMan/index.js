d04fa31c6e95bbcbb725d2afd175ea15a9c4231f
chocko120 <chocko120@gmail.com>
2019-03-02 20:08:38 +0200

    some css

---
+++ src/components/HangMan/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HangMan.css';
+
+const HangMan = () => {
+  return (
+    <div>
+      <div className="circle">
+      </div>
+      <div className='body-wrap'><div className="main-body"></div></div>
+    </div>
+  );
+};
+
+export default HangMan;
