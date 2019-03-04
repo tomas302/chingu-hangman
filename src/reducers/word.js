f69c23842fabd533e4971e78aee44116fb8e159e
Tomás Dornell <tomasdornell.gtd@gmail.com>
2019-03-04 22:48:23 +0100

    Removed everything

--- src/reducers/word.js
+++
@@ -1,9 +0,0 @@
-export default function word(state = "", action) {
-    switch(action.type) {
-        case('SET_WORD'):
-            let regex = /((?![A-z]).)*/g;
-            return action.word.replace(regex, "");
-        default:
-            return state;
-    }
-};
