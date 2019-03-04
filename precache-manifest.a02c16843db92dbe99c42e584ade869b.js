e2b70bd3fb9795c6f6645a18f4466074441991fb
Thomas Dornell Bonilla <thomasdornell@gmail.com>
2019-03-04 22:49:41 +0100

    Updates

---
+++ precache-manifest.a02c16843db92dbe99c42e584ade869b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a9c602227dba65af322",
+    "url": "/chingu-hangman/static/css/main.94943bd5.chunk.css"
+  },
+  {
+    "revision": "0a9c602227dba65af322",
+    "url": "/chingu-hangman/static/js/main.0a9c6022.chunk.js"
+  },
+  {
+    "revision": "9d996fd2136ec75a4868",
+    "url": "/chingu-hangman/static/js/runtime~main.9d996fd2.js"
+  },
+  {
+    "revision": "12c3d7e70cf5b05c1038",
+    "url": "/chingu-hangman/static/css/2.fc00aea3.chunk.css"
+  },
+  {
+    "revision": "12c3d7e70cf5b05c1038",
+    "url": "/chingu-hangman/static/js/2.12c3d7e7.chunk.js"
+  },
+  {
+    "revision": "01cc9e5adede96c168aae3c4035a3f2d",
+    "url": "/chingu-hangman/index.html"
+  }
+];
